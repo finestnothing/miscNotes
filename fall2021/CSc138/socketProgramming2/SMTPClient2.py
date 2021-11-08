23fad37e003772a932717432863c929279298d9b
Alec_Desktop <iforgot52@gmail.com>
2021-11-07 17:54:18 -0800

    Finished CSc 138 Socket Programming Assignment2

--- fall2021/CSc138/socketProgramming2/SMTPClient2.py
+++ fall2021/CSc138/socketProgramming2/SMTPClient2.py
@@ -11,37 +11,45 @@ port = 25
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((mailserver, port))
 
-recv = clientSocket.recv(1024)
+recv = clientSocket.recv(1024).decode()
 print(recv)
-if recv[:3] != b'220':
-    print('220 reply not received from server.')
 
 # Send HELO command and print server response.
 heloCommand = 'HELO Alice\r\n'
-clientSocket.send(heloCommand)
-recv1 = clientSocket.recv(1024)
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
 print(recv1)
-if recv1[:3] != b'250':
-    print('250 reply not received from server.')
 
 # Send MAIL FROM command and print server response.
+mailFromCommand = 'MAIL FROM: <alecresha@csus.edu>\r\n'
+clientSocket.send(mailFromCommand.encode())
+recv2 = clientSocket.recv(1024).decode()
+print(recv2)
 
 # Send RCPT TO command and print server response.
+rcptToCommand = 'RCPT TO: <iforgot52@gmail.com>\r\n'
+clientSocket.send(rcptToCommand.encode())
+recv3 = clientSocket.recv(1024).decode()
+print(recv3)
 
 # Send DATA command and print server response.
-
-print(recv)
-if recv[:3] != b'250':
-    print('250 reply not received from server.')
+dataCommand = 'DATA\r\n'
+clientSocket.send(dataCommand.encode())
+recv4 = clientSocket.recv(1024).decode()
+print(recv4)
 
 # Send message data.
+clientSocket.send(msg.encode())
 
 # Message ends with a single period.
+clientSocket.send(endmsg.encode())
 
 # Send QUIT command and get server response.
+quitCommand = 'QUIT\r\n'
+clientSocket.send(quitCommand.encode())
+recv5 = clientSocket.recv(1024).decode()
+print(recv5)
 
 # Fill in end
 
 print(recv)
-if recv[:3] != b'221':
-    print('221 reply not received from server.')
